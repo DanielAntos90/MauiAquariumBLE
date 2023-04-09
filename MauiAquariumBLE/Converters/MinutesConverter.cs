e0ac9822b27c574d1de775a4b9d0cb52547a9379
Daniel Antoš <dan.antos@seznam.cz>
2023-04-09 14:06:41 +0200

    refactor: view for android

---
+++ MauiAquariumBLE/Converters/MinutesConverter.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MauiAquariumBLE.Converters
+{
+    public class MinutesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int minutes)
+            {
+                return $"{minutes} min.";
+            }
+            else if (value is string text)
+            {
+                if (int.TryParse(text.Replace(" min.", ""), out int minutes2))
+                {
+                    return minutes2;
+                }
+            }
+            return value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int minutes)
+            {
+                return $"{minutes} min.";
+            } else if (value is string text)
+            {
+                if (int.TryParse(text.Replace(" min.", ""), out int minutes2))
+                {
+                    return minutes2;
+                }
+            }
+            return value;
+        }
+    }
+
+}
