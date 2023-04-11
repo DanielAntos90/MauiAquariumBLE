5a8768800ce2a034fe4ac8e60945c45121623389
Daniel Antoš <dan.antos@seznam.cz>
2023-04-11 15:05:38 +0200

    fix: null reference

--- MauiAquariumBLE/ViewModels/HomeViewModel.cs
+++ MauiAquariumBLE/ViewModels/HomeViewModel.cs
@@ -100,7 +100,7 @@ public partial class HomeViewModel : BaseViewModel
         IsWorking = true; //TODO use custom event for IsWorking
         IsDataReceived = false;
         await BluetoothLEService.ConnectToDeviceAsync();
-        if(BluetoothLEService.Device.Name != null)
+        if(BluetoothLEService.Device?.Name != null)
         {
             Title = BluetoothLEService.Device.Name;
         }
